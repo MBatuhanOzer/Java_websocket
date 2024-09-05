cd85d178a3f61815ee13b3bba0a314ffdf465995
Batuhan <batuhanozer1042@gmail.com>
2024-09-05 14:15:33 +0300

    Initilazing

---
+++ src/test/java/com/example/Java_websocket/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
