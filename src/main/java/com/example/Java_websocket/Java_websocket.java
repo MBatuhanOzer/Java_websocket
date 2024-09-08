2ebae769d8ec28f9649cfb8b1bfd27349eb42b07
Batuhan <batuhanozer1042@gmail.com>
2024-09-08 17:19:06 +0300

    Rename

--- src/main/java/com/example/Java_websocket/Java_websocket.java
+++ src/main/java/com/example/Java_websocket/Java_websocket.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Java_websocket {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Java_websocket.class, args);
 	}
 
 }
