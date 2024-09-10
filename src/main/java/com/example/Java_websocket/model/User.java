83131aff0f59cf45cfb43c42ae94abac32d31bbb
Batuhan <batuhanozer1042@gmail.com>
2024-09-10 15:21:50 +0300

    database config

--- src/main/java/com/example/Java_websocket/model/User.java
+++ src/main/java/com/example/Java_websocket/model/User.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
@@ -14,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetails;
  * database.
  */
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
 
     /**
